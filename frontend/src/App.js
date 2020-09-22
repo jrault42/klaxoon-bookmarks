c3b451b2da67c1027e57fe4eeaf92f357551ac38
jrault42 <jeanne.rault42@gmail.com>
2020-09-22 23:12:53 +0200

    get real bookmarks frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 import Table from './Table';
 import AddForm from './AddForm';
+
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = { };
-    this.bookmarks = [
-      {
-        id: '0',
-        url: 'titi0',
-        author: 'titi0',
-        title: 'titi0',
-        createDate: 'titi0'
-      },
-      {
-        id: '1',
-        url: 'titi1',
-        author: 'titi1',
-        title: 'titi1',
-        createDate: 'titi1'
-      }
-    ];
-
     this.handleAddBookmark = this.handleAddBookmark.bind(this);
   }
+
+   componentDidMount() {
+    fetch('http://localhost:8080/bookmarks')
+      .then(res => res.json())
+      .then(
+        (bookmarks) => {
+          this.setState({
+            isLoaded: true,
+            bookmarks
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
 
   handleAddBookmark (evt) {
     evt.stopPropagation();
@@ -37,18 +44,24 @@ class App extends Component {
   }
 
   render () {
-    console.log("COUCOU", this.state)
-    if (this.state.addBookmark) {
+    const {bookmarks, error, isLoaded} = this.state;
+      if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else if (this.state.addBookmark) {
       return (
         <div className='App mt-2'>
          <AddForm></AddForm>
         </div>
       );
+    } else if (!bookmarks.length) {
+        return <AddForm></AddForm>
     } else {
         return (
         <div className='App mt-2'>
           <button className='btn btn-success m-2' onClick={this.handleAddBookmark}>Ajouter</button>
-          <Table bookmarks={this.bookmarks} />,
+          <Table bookmarks={this.state.bookmarks} />,
         </div>
       );
     }
